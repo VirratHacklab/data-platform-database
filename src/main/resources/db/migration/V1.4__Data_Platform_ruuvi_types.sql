817f0fe32e66f04795ca288d5781ab02d8bd1bf7
Antti Rintala <antti@rintala.it>
2019-10-30 01:52:03 +0200

    Drop before alter

--- src/main/resources/db/migration/V1.4__Data_Platform_ruuvi_types.sql
+++ src/main/resources/db/migration/V1.4__Data_Platform_ruuvi_types.sql
@@ -1,1 +1,11 @@
+DROP TABLE ruuvi_telemetry;
+
 ALTER TYPE ruuvi ALTER ATTRIBUTE humidity TYPE FLOAT;
+
+CREATE TABLE ruuvi_telemetry (
+    device_id INTEGER REFERENCES device (id),
+    time TIMESTAMP WITH TIME ZONE NOT NULL,
+    parameters ruuvi
+);
+
+SELECT create_hypertable('ruuvi_telemetry', 'time');
